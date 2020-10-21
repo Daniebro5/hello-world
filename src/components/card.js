7039afd500c9ffcef028c43713df8d3df5692f62
Danni Brito <danni.brito@devsu.com>
2020-10-20 20:39:53 -0500

    button

--- src/components/card.js
+++ src/components/card.js
@@ -2,14 +2,21 @@ import React from "react";
 
 export default function Tarjeta(props) {
     return (
-        <div className="md:w-64">
+        <div className="md:w-64 px-4">
             <img className="w-full h-56" src="https://source.unsplash.com/random"/>
-            <h1 className="font-bold lg:text-xl">
-                {props.titulo}
-            </h1>
-            <h3 className="text-gray-600 md:text-red-300 lg:text-blue-500">
-                {props.canal}
-            </h3>
+            <div className="flex justify-between pt-4">
+                <div>
+                    <h1 className="font-bold lg:text-xl">
+                        {props.titulo}
+                    </h1>
+                    <h3 className="text-gray-600 md:text-red-300 lg:text-blue-500">
+                        {props.canal}
+                    </h3>
+                </div>
+                <button className="rounded-full border bg-orange-500 text-white px-2">Compartir</button>
+            </div>
+            
+            
             <div className="flex space-x-2 text-gray-600">
                 <h3>
                     {props.numeroVisualizaciones}
--- src/components/cardGrid.js
+++ src/components/cardGrid.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardGrid(props){
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-6">
             {props.children}
         </div>
     )
